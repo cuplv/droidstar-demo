c90664a8e38cf7eae5abb91c24333966690d2441
octalsrc <nicklewchenko92@gmail.com>
2018-07-26 00:19:17 -0600

    Finish on end of experiment

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -19,16 +19,22 @@ import Network.DNS.Resolver
 import Network.DNS.Lookup
 import Network.DNS.Types (DNSError)
 
+import qualified System.IO as IO
+import qualified System.Process as Proc
+
 import Turtle
 import qualified Control.Foldl as Foldl
 
 main :: IO ()
 main = do 
   putStrLn "Starting..."
+  IO.hFlush IO.stdout
   ip <- getIP
+  -- let ip = Right ["127.0.0.1"]
   case ip of
     Right [ipaddr] -> 
-      do connectAdb ipaddr
+      do Concurrent.threadDelay (1000 * 1000 * 120)
+         connectAdb ipaddr
          state <- Concurrent.newMVar []
          Warp.run 30025 $ WS.websocketsOr
            WS.defaultConnectionOptions
@@ -45,11 +51,15 @@ getIP = do
 connectAdb :: String -> IO ()
 connectAdb ip = do
   (_,o) <- procStrict "adb" ["connect",Text.pack ip] empty
-  if Text.isPrefixOf "unable" o
+  if Text.isInfixOf "unable" o
      then do putStrLn "Emulator was not ready, retrying in 10s..."
+             IO.hFlush IO.stdout
              Concurrent.threadDelay (1000 * 1000 * 10)
              connectAdb ip
-     else putStrLn "Connected to emulator."
+     else do putStrLn "Connected to emulator."
+             -- putStrLn (show (Text.unpack o))
+             -- putStrLn "That's what it said."
+             IO.hFlush IO.stdout
 
 httpApp :: Wai.Application
 httpApp _ respond = respond $ Wai.responseLBS Http.status400 [] "Not a websocket request"
@@ -81,26 +91,59 @@ listen conn clientId stateRef = Monad.forever $ do
   let send = WS.sendTextData conn
   case msg of
 
-    "req:AsyncTask" -> do 
-      send "dbg:Setting up..."
-      send "dbg:Asdf1"
-      send "dbg:Asdf2"
-      send "dbg:Asdf3"
-      send "dbg:Asdf4"
-      send "dbg:Asdf5"
-      send "dbg:Asdf6"
-      send "dbg:Asdf7"
-      send "dbg:Asdf8"
-      stdout (inproc "echo" ["Doing a thing..."] empty)
-      file <- readFile "./a.txt"
-      send . Text.pack $ ("rsl:" ++ file)
+    -- "req:AsyncTask" -> do 
+    --   send "dbg:Setting up..."
+    --   send "dbg:Asdf1"
+    --   send "dbg:Asdf2"
+    --   send "dbg:Asdf3"
+    --   send "dbg:Asdf4"
+    --   send "dbg:Asdf5"
+    --   send "dbg:Asdf6"
+    --   send "dbg:Asdf7"
+    --   send "dbg:Asdf8"
+    --   Turtle.stdout (inproc "echo" ["Doing a thing..."] empty)
+    --   file <- readFile "./a.txt"
+    --   send . Text.pack $ ("rsl:" ++ file)
 
-    "req:CountDownTimer" -> do
-      send "dbg:Running experiment..."
-      proc "adb" ["install","/root/droidstar-debug.apk"] empty
-      stdout (inproc "adb" ["shell","am","start","-a","android.intent.action.MAIN","-n","edu.colorado.plv.droidstar.experiments/.MainActivity"] empty)
-      foldIO (grep (choice [has "DROIDSTAR",has "STARLING"]) (inshell "adb logcat" empty)) (Foldl.mapM_ ((\a -> send a >> print a) . ("dbg:" <>) . lineToText))
-    
+    "req:CountDownTimer" -> experiment send "/root/droidstar-debug.apk"
+      -- send "dbg:Running experiment..."
+      -- proc "adb" ["install","/root/droidstar-debug.apk"] empty
+      -- Turtle.stdout (inproc "adb" ["shell","am","start","-a","android.intent.action.MAIN","-n","edu.colorado.plv.droidstar.experiments/.MainActivity"] empty)
+      -- followLog send
+      -- send "dbg:All done."
+
+experiment send f = do
+  send "dbg:Running experiment..."
+  proc "adb" ["install",f] empty
+  Turtle.stdout (inproc "adb" ["shell","am","start","-a","android.intent.action.MAIN","-n","edu.colorado.plv.droidstar.experiments/.MainActivity"] empty)
+  followLog send
+  send "dbg:All done."
+  Turtle.shell 
+    "adb pull /sdcard/Android/data/edu.colorado.plv.droidStar.experiments/files/results ./"
+    empty
+
+
+followLog :: (Text -> IO ()) -> IO ()
+followLog send = logcatDS >>= r
+  where r (n,k) = do l <- n
+                     if not (Text.isInfixOf "Completed learning" l)
+                        then do let l' = "dbg:" <> l
+                                send l'
+                                print l'
+                                r (n,k)
+                        else k
+
+logcatDS :: IO (IO Text,IO ())
+logcatDS = do (_,Just hout,_,ph) <- Proc.createProcess 
+                                      (Proc.proc "adb" ["logcat"]){ Proc.std_out = Proc.CreatePipe}
+              let next = do l <- Text.pack <$> IO.hGetLine hout
+                            if or [Text.isInfixOf "DROIDSTAR" l
+                                  ,Text.isInfixOf "STARLING" l]
+                               then return l
+                               else next
+              let kill = Proc.terminateProcess ph >> IO.hClose hout
+              return (next,kill)
+
 wsApp :: Concurrent.MVar State -> WS.ServerApp
 wsApp stateRef pendingConn = do
   conn <- WS.acceptRequest pendingConn
